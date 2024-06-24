33e3eb20a122af7312515c028bf6d5269ae38493
Sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2024-06-24 04:42:47 +0300

    feat: add all files for the based project

---
+++ src/main/java/mg/nantenaina/Pointing_System/PointingSystemApplication.java
@@ -0,0 +1,13 @@
+package mg.nantenaina.Pointing_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/nantenaina/Pointing_System/PointingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.nantenaina.Pointing_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
